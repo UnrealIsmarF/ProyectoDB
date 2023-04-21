ee56c8a90d89d47d97a476632f6808401b7f11b6
umg <umg@HUEF2.labhue.local>
2023-04-20 20:41:35 -0600

    123

--- src/Controladores/ConexionControler.java
+++ src/Controladores/ConexionControler.java
@@ -2,19 +2,26 @@
 package Controladores;
 
 import Modelos.Coneccion;
+import Modelos.PersonaModel;
 import Vistas.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
 
 public class ConexionControler implements ActionListener{
     frmLogIn VistaLogin;
     frmPrincipal VistaPrincipal;
     Coneccion ModeloConexion;
-
-    public ConexionControler(frmLogIn VistaLogin, frmPrincipal VistaPrincipal, Coneccion ModeloConexion) {
+    frmPersonas VistaPersonas;
+    PersonaModel ModeloPersona;
+    
+    public ConexionControler(frmLogIn VistaLogin, frmPrincipal VistaPrincipal, frmPersonas VistaPersonas, Coneccion ModeloConexion, PersonaModel ModeloPer) {
         this.VistaLogin = VistaLogin;
         this.VistaPrincipal = VistaPrincipal;
+        this.VistaPersonas = VistaPersonas;
         this.ModeloConexion = ModeloConexion;
+        this.ModeloPersona = ModeloPer;
+
         //LEVANTAR FORMULARIO PRINCIPAL
         this.VistaPrincipal.setExtendedState(frmPrincipal.MAXIMIZED_BOTH);
         this.VistaPrincipal.setVisible(true);
@@ -31,6 +38,20 @@ public class ConexionControler implements ActionListener{
        
        this.ModeloConexion.Conectar(this.VistaLogin.txtUser.getText(),
                this.VistaLogin.txtPass.getText());
+       
+       if(ModeloConexion!=null)
+       {
+           this.VistaLogin.dispose();
+           this.VistaPersonas.btnAceptar.addActionListener(this);
+           this.VistaPersonas.setLocationRelativeTo(null);
+           this.VistaPersonas.setVisible(true);
+       }
+       if(arg0.getSource()==this.VistaPersonas.btnAceptar)
+       {
+        //Mandar a guardar Registro
+        this.ModeloPersona.GuardarPersona(this.VistaPersonas.txtApellido.getText(),this.VistaPersonas.txtNombre.getText(),
+                this.VistaPersonas.txtTelefono.getText());
+       }
        
     }
 }
